04e808793012f247c0153ff7711d4052b70fbab9
Wisley Moracha <wisleymoracha196@gmail.com>
2025-10-08 17:26:31 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET || "supersecretkey";
